1032122928ac1b28be275f032b35f52b7be138f3
kimjeonghyeon <kr100475@gmail.com>
2024-01-23 10:58:04 +0900

    [delete] 필요없는 파일 삭제

--- test.java
+++
@@ -1,9 +0,0 @@
-
-public class test {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-        System.out.println("Hello?");
-        System.out.println("???");
-    }
-}
