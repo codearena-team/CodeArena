e83d917928d7c9460df8194d6608415297c1d0e0
kimjeonghyeon <kr100475@gmail.com>
2024-01-23 10:38:15 +0900

    [feat] Hello World 테스트 수정

--- test.java
+++ test.java
@@ -1,8 +1,8 @@
-import java.io.IOException;
 
 public class test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("Hello?");
     }
 }
