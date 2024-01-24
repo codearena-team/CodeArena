5b254a28d26393302d15d0f3b3282159655ac3d6
장세정 <wkdtpwjd29@naver.com>
2024-01-24 17:51:07 +0900

    [Feat]상단바,로그인ui구현

--- front-end/src/components/navBar.jsx
+++ front-end/src/components/navBar.jsx
@@ -2,37 +2,50 @@ import { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Logo from '../images/logo.png'
 
-const navigation = [
-  { name: 'home', href: '/', current: false },
-  { name: 'submit', href: '/submit', current: false },
-  { name: 'create', href: '/create', current: false },
-  { name: 'problemSolve', href: '/problemsolve', current: false },
-  { name: 'arena', href: '/arena', current: false },
-  { name: 'board', href: '/board', current: false },
+const navigation = [ 
+  { name: 'test', href: '/test', current: false },
   { name: 'Login', href: '/login', current: false },
+  { name: 'Logout', href: '/logout', current: false },
+  { name: 'Profile', href: '/profile', current: false },
+  { name: 'home', href: '/', current: false },
+  // { name: 'submit', href: '/submit', current: false },
+  // { name: 'create', href: '/create', current: false },
+  { name: 'problem', href: '/problem', current: false },
+  { name: 'Community', href: '/Community', current: false },
+  { name: 'Arena', href: '/arena', current: false },
+ 
 ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function NavBar() {
   return (
     <Disclosure as="nav" className="">
       {({ open }) => (
         <>
-          <div className=" px-2 sm:px-2 lg:px-2">
-            <div className="relative flex h-16 items-center justify-end">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          
+            <div className="relative flex h-16 justify-start">
+              <div className="flex-initial absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                
+                {/* <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button> */}
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-1 justify-between">
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    className="h-full w-auto"
+                    src={Logo}
                     alt="Your Company"
                   />
                 </div>
@@ -40,10 +53,10 @@ export default function Example() {
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <Link
-                        key={item.name}
+                        key={item.name} 
                         to={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : ' hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white' : 'text-xl hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -57,17 +70,18 @@ export default function Example() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  className="relative rounded-full p-1 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  {/* <span className="absolute -inset-1.5" /> */}
+                  {/* <span className="sr-only">View notifications</span> */}
+                  <BellIcon className="h-6 w-6" aria-hidden="true" /> 
+                  
                 </button>
 
-                {/* Profile dropdown */}
+             
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    {/* <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
@@ -75,7 +89,7 @@ export default function Example() {
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                       />
-                    </Menu.Button>
+                    </Menu.Button> */}
                   </div>
                   <Transition
                     as={Fragment}
@@ -90,8 +104,7 @@ export default function Example() {
                 </Menu>
               </div>
             </div>
-          </div>
-
+     
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
--- front-end/src/pages/login/Login.jsx
+++ front-end/src/pages/login/Login.jsx
@@ -1,8 +1,43 @@
+import { useState } from 'react'
+import CustomInput from '../../components/common/CustomInput'
+import './login.css'
+
 export default function Login() {
+  // const [email, setEmail] = useState()
+  // function onclick () {
+  //   const payload ={
+  //     email : email,
+  //     password : password
+  //   }
+  //   requestLogin(payload)
+  // }
   return (
-    <div>
-      <h1>로그인</h1>
+    <div className='flex justify-center'>
+      {/* <img src={pink} alt="" 
+      style={{width:350, height:400, marginTop:-100, marginLeft:'25%'}}
+       />
+       */}
+       
+       
+       <div className='whitebox' style={{backgroundColor:'white',marginTop:-100, padding:50}}>
+       <div style={{width:400,height:800,backgroundColor: '#E6BAA3', marginTop: -350,marginLeft:-450, borderBottomLeftRadius:50}}></div>
+       <div className='whitebox' style={{ backgroundColor: '#F4ECE4', marginTop: -980,marginLeft:-300}}></div>
+        <div>
+          <CustomInput 
+          label="Email Id"
+          // setEmail={setEmail}
+          />
+          <CustomInput 
+          label="Password"
+          type="password"
+          />
+        </div>
+
+
+       </div>
     </div>
+     
+  
   );
 }
   
