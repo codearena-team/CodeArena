349b9da800483f0ca1e7731b0daa211bd41f168a
김회창 <khc9812121@gmail.com>
2024-01-31 14:01:11 +0900

    [fix] 빌드 시 contextLoad 에러발생고치기위한 쓸데없는 어노테이션 제거

--- back-end/codearena/src/test/java/com/ssafy/codearena/CodearenaApplicationTests.java
+++ back-end/codearena/src/test/java/com/ssafy/codearena/CodearenaApplicationTests.java
@@ -3,7 +3,6 @@ package com.ssafy.codearena;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CodearenaApplicationTests {
 
 	@Test
