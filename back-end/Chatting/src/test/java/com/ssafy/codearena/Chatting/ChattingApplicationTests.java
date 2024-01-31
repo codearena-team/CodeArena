8140e4d10e5e4e0eae7beb8e026212968e22f6ec
김회창 <khc9812121@gmail.com>
2024-01-31 15:56:27 +0900

    [fix] test 어노테이션 제거

--- back-end/Chatting/src/test/java/com/ssafy/codearena/Chatting/ChattingApplicationTests.java
+++ back-end/Chatting/src/test/java/com/ssafy/codearena/Chatting/ChattingApplicationTests.java
@@ -3,7 +3,6 @@ package com.ssafy.codearena.Chatting;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ChattingApplicationTests {
 
 	@Test
