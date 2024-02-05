c968704ead5c2193bc79f306ba8c87686c4cd30f
김정현 <kr100475@gmail.com>
2024-02-05 21:47:47 +0900

    [feat] 경쟁 아레나 로직 적용

--- back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/mapper/GameMapper.java
+++ back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/mapper/GameMapper.java
@@ -1,8 +1,17 @@
 package com.ssafy.codearena.Chatting.mapper;
 
+import com.ssafy.codearena.Chatting.dto.GameCreateDto;
+import com.ssafy.codearena.Chatting.dto.GameInfoDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface GameMapper {
-    public int findProblemById();
+    public int findProblemById() throws Exception;
+    public GameInfoDto findRoomById(String gameId) throws Exception;
+    public List<GameInfoDto> findAllRoom(Map<String, Object> map) throws Exception;
+    public int getTotalGameCount(Map<String, Object> map) throws Exception;
+    public void createPrivateRoom(GameCreateDto gameCreateDto) throws Exception;
 }
