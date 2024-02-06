6a359c845e397616fc75332e015a2b5065db7f87
김정현 <kr100475@gmail.com>
2024-02-06 15:52:55 +0900

    [fix] WebConfig 수정

--- back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/config/WebConfig.java
+++ back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
