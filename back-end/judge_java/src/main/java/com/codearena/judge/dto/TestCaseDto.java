975cb3134b859c90e4ae9fb0667e27a88e3e286a
오승현 <timber3@naver.com>
2024-02-06 17:49:23 +0900

    [feat] java 채점 validation 체크

--- back-end/judge_java/src/main/java/com/codearena/judge/dto/TestCaseDto.java
+++ back-end/judge_java/src/main/java/com/codearena/judge/dto/TestCaseDto.java
@@ -1,8 +1,10 @@
 package com.codearena.judge.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TestCaseDto {
     private String input;
     private String output;
