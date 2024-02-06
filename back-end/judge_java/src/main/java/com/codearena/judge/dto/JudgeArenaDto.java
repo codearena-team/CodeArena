975e10852251100177fefeaf0b739c445ce8bbfa
오승현 <timber3@naver.com>
2024-02-06 21:49:13 +0900

    [fix] 토큰 speed eff 수정

---
+++ back-end/judge_java/src/main/java/com/codearena/judge/dto/JudgeArenaDto.java
@@ -0,0 +1,11 @@
+package com.codearena.judge.dto;
+
+import lombok.Data;
+
+@Data
+public class JudgeArenaDto {
+    private String submitId;
+    private String userId;
+    private String problemId;
+    private String userCode;
+}
---
+++ back-end/judge_java/src/main/java/com/codearena/judge/dto/JudgeNormalDto.java
@@ -0,0 +1,11 @@
+package com.codearena.judge.dto;
+
+import lombok.Data;
+
+@Data
+public class JudgeNormalDto {
+    private String submitId;
+    private String userId;
+    private String problemId;
+    private String userCode;
+}
