b88cecdbe1e29825913b5c50798d1722ef3fbe1c
김정현 <kr100475@gmail.com>
2024-02-08 19:08:02 +0900

    [modity] DTO 변수 수정

--- back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/dto/CompetitiveManageDto.java
+++ back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/dto/CompetitiveManageDto.java
@@ -8,8 +8,6 @@ public class CompetitiveManageDto implements Comparable<CompetitiveManageDto>{
     private int Participants;
     private String player1;
     private String player2;
-    private String player3;
-    private String player4;
     private String roomType;
     private String gamemode;
     private boolean player1_leave;
--- back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/service/ChatServiceImpl.java
+++ back-end/Chatting/src/main/java/com/ssafy/codearena/Chatting/service/ChatServiceImpl.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Slf4j
