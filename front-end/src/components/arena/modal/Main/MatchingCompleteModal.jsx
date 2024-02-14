db4fd3bbcf2c47042ee57b94d879efa25608dcc2
이귀현 <dlrnlgus7660@naver.com>
2024-02-14 18:05:11 +0900

    [modfiy] 탑배너 기능및UI추가 구성중

--- front-end/src/components/arena/modal/Main/MatchingCompleteModal.jsx
+++ front-end/src/components/arena/modal/Main/MatchingCompleteModal.jsx
@@ -7,6 +7,27 @@ export default function MatchingCompleteModal({ matchingState, onAccept, onCance
   const [userNicknames, setUserNicknames] = useState('');
   const [enemyNicknames, setEnemyNicknames] = useState('');
 
+  const [waitingText, setWaitingText] = useState("");
+  const [rejectingText, setRejectingText] = useState("");
+
+  const handleAccept = () => {
+    
+    // 여기서 서버로 수락 메세지를 보내고 상태를 업데이트합니다.
+    onAccept();
+
+    // 추가: 수락 후 대기 중 문구 설정
+    setWaitingText("상대방의 수락을 기다리고 있어요!");
+  };
+
+  const handleReject = () => {
+
+    // 여기서 서버로 거절 메세지를 보내고 상태를 업데이트합니다.
+    onCancel();
+
+    // 추가: 거절 후 매칭 재시도 문구 설정
+    setRejectingText("상대가 매칭을 거절하여 새로운 매칭을 시도중입니다...");
+  };
+
   useEffect(() => {
     console.log('내려받은 유저 닉네임, 이미지 확인 :', matchingState);
     setUserImgSrcs(matchingState.userImgSrc);
@@ -51,6 +72,20 @@ export default function MatchingCompleteModal({ matchingState, onAccept, onCance
           거절
         </button>
       </div>
+
+      {/* 수락 후 대기 중 문구 */}
+      {waitingText && (
+        <div className="text-center mt-4 text-gray-500">
+          {waitingText}
+        </div>
+      )}
+
+      {/* 거절 후 매칭 재시도 문구 */}
+      {rejectingText && (
+        <div className="text-center mt-4 text-gray-500">
+          {rejectingText}
+        </div>
+      )}
     </div>
   );
 }
